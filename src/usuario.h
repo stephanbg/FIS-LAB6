6b93b57e2d357cc34d29d4977ba12782f4cc96a7
Adrián Abreu Alonso <adrianabreualonso2001@gmail.com>
2023-05-07 01:34:44 +0100

    Update usuario.h

--- src/usuario.h
+++ src/usuario.h
@@ -1,5 +1,5 @@
 #pragma once
-
+/* Clase que define la función del usuario en el sistema */
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -8,15 +8,20 @@
 
 class Usuarios {
  public:
+ //Constructores
   Usuarios() {}
   Usuarios(const std::string&, const std::string&);
+ // Métodos
   void Menu(const std::string&) const;
   std::string DimeUnNombreDeCerradura() const;
+ // Getters
   std::string get_id() const { return id_; }
   std::string& set_id() { return id_; }
   std::string get_passwd() const { return passwd_; }
  private:
+ // Atributos
   std::string id_{};
   std::string passwd_{};
+ // Método
   bool AbrirPuerta(const std::string&, const std::string&) const;
 };
